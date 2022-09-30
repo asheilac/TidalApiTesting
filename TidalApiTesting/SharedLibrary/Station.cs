e29416c15e08e616b83a68dfe6ba9c4edcad54c0
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-09-30 15:41:02 +0100

    assert on content response of GetStation

---
+++ TidalApiTesting/SharedLibrary/Station.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class Crs
+    {
+        public string Type { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public bool ContinuousHeightsAvailable { get; set; }
+        public string Footnote { get; set; }
+    }
+
+    public class Root
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+        public List<double> bbox { get; set; }
+        public Crs crs { get; set; }
+    }
+}
--- TidalApiTesting/TidalApiTesting/ApiTests.cs
+++ TidalApiTesting/TidalApiTesting/ApiTests.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Web;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using SharedLibrary;
 
 namespace TidalApiTesting
@@ -10,7 +11,7 @@ namespace TidalApiTesting
     public class Tests
     {
         [Test]
-        public async Task GetStationsReturnsOK()
+        public async Task GetStationsReturnsSuccessful()
         {
             var config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.development.json")
@@ -30,6 +31,7 @@ namespace TidalApiTesting
             var response = await client.GetAsync(uri);
             response.Should().BeSuccessful();
         }
+
         [Test]
         public async Task GetStationsReturnsUnauthorised()
         {
@@ -45,5 +47,56 @@ namespace TidalApiTesting
             response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
 
+        [Test]
+        public async Task GetStationReturnsSuccessful()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.development.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            using var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", $"{settings.ApiKey}");
+
+            queryString["name"] = "{string}";
+            var uri = "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0322?" + queryString;
+
+            var response = await client.GetAsync(uri); 
+            response.Should().BeSuccessful();
+        }
+
+        [Test]
+        public async Task GetStationReturnsStationName()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.development.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            using var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", $"{settings.ApiKey}");
+
+            queryString["name"] = "{string}";
+            var uri = "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/0322?" + queryString;
+
+            var response = await client.GetAsync(uri);
+            var content = await DeserializeContent(response);
+            content.properties.Name.Should().Be("Hirta (Bagh A' Bhaile)");
+        }
+        
+        private async Task<Root> DeserializeContent(HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var contentDeserialized = JsonConvert.DeserializeObject<Root>(content);
+            return contentDeserialized;
+        }
     }
 }
