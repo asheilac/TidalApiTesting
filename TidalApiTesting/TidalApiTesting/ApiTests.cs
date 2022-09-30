d7e5e719bdbbe196b812e1e67cf6350d84fc9c62
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-09-30 14:49:42 +0100

    added configuration

--- TidalApiTesting/TidalApiTesting/ApiTests.cs
+++ TidalApiTesting/TidalApiTesting/ApiTests.cs
@@ -1,6 +1,9 @@
+using System.Configuration;
 using System.Net;
 using System.Web;
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using SharedLibrary;
 
 namespace TidalApiTesting
 {
@@ -9,10 +12,17 @@ namespace TidalApiTesting
         [Test]
         public async Task GetStationsReturnsOK()
         {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.development.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
             using var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", $"{settings.ApiKey}");
 
             queryString["name"] = "{string}";
             var uri = "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations?" + queryString;
